8506f7c46c47b3c8a5ca45dde3190dd5d82a052e
Mehdi <elwafi.courrier99@gmail.com>
2023-02-28 20:20:29 +0100

    Added .gitignore file and modified test_pnote to include setup and tearDown class methdos

--- test_pnote.py
+++ test_pnote.py
@@ -1,14 +1,24 @@
 import unittest
-from pnote import load_csv_database,create_note,delete_note,describe_note,generate_note_id
 import csv
 import os
 
+from pnote import *
 
 class TestPnote(unittest.TestCase):
     
     @classmethod
     def setUpClass(cls):
-        pass
+        self.working_dir = os.path.join('.','./fixtures/')
+        self.db_file = os.path.join(working_dir,'testdb.csv')
+        load_csv_database(self.working_dir,self.db_file)
+    
+    @classmethod
+    def tearDownClass(cls):
+        os.removedirs(self.work)
+    
+    
+    def test_load_csv_database(self):
+        self.assert
 
 if __name__ == '__main__':
     unittest.main()
