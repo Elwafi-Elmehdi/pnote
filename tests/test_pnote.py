6a8dfa5db6d0d12851a2c618229ec21d337b037d
Mehdi <elwafi.courrier99@gmail.com>
2023-02-28 14:27:05 +0100

    Added test module for pnote module

---
+++ tests/test_pnote.py
@@ -0,0 +1,15 @@
+import unittest
+import csv
+import sys
+import os
+
+
+class TestPnote(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        with open('test_db.csv','w') as f:
+            
+
+if __name__ == '__main__':
+    unittest.main()
