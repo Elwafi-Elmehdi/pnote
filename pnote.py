43ef4ef799be569aa39470a0f1cb56e82c2b29da
Mehdi <elwafi.courrier99@gmail.com>
2023-03-01 12:21:21 +0100

    Refactored pnote functions to remove cli interaction logic and added returns

--- pnote.py
+++ pnote.py
@@ -51,7 +51,8 @@ def list_notes(csv_path):
     with open(csv_path,'r') as db_f:
         csv_reader = csv.DictReader(db_f)
         for note in csv_reader:
-            print(f"{  note['id'] } \t {note['datetime'] } \t { note['title'] } \t { note['body'] }\n")
+            # print(f"{  note['id'] } \t {note['datetime'] } \t { note['title'] } \t { note['body'] }\n")
+            pass
             
 def generate_note_id(csv_path):
     ids = [0]
@@ -73,48 +74,47 @@ def describe_note(note_id,db_f):
         csv_reader = csv.DictReader(f)
         for note in csv_reader:
             if int(note["id"]) == note_id:
-                print(f"{ note['id'] }\t {note['datetime'] } \t { note['title'] } \n\n{ note['body'] }\n")
+                return note
+                # print(f"{ note['id'] }\t {note['datetime'] } \t { note['title'] } \n\n{ note['body'] }\n")
 
 def delete_note(note_id,db_f):
-    describe_note(note_id,db_f)
-    prompt = input("Are you sure you want to delete this note? [Yes-No]:")
-    if prompt == 'Yes':
-        with open(db_f,'r') as f:
-            lines = f.readlines()
-            
-        for line in lines:
-            if line.startswith('id'):
-                continue
-            values = line.split(',')
-            print(values)
-            if int(values[0]) == note_id:
-                lines.remove(line)
-                break
-
-        with open(db_f,'w') as f:
-            f.writelines(lines)
-    else:
-        pass
+    # describe_note(note_id,db_f)
+    # prompt = input("Are you sure you want to delete this note? [Yes-No]:")
+    # if prompt == 'Yes':
+    note = None
+    with open(db_f,'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        if line.startswith('id'):
+            continue
+        values = line.split(',')
+        if int(values[0]) == note_id:
+            lines.remove(line)
+            note = {'id':values[0],'datetime':values[1],'title':values[2],'body':values[3]}
+            break
+    with open(db_f,'w') as f:
+        f.writelines(lines)
+    return note
 
 
-load_csv_database(working_dir,db_file)
+# load_csv_database(working_dir,db_file)
 
-if len(arguments) < 1:
-    usage()
+# if len(arguments) < 1:
+#     usage()
 
-match arguments[0]:
-    case "ls":
-        list_notes(db_file)
-        exit(0)
-    case "desc":
-        describe_note(int(arguments[1]),db_file)
-        exit(0)
-    case "new":
-        create_note(db_file,arguments[1],arguments[2])
-    case "rm":
-        delete_note(int(arguments[1]),db_file)
-        exit(0)
-    case _:
-        usage()
+# match arguments[0]:
+#     case "ls":
+#         list_notes(db_file)
+#         exit(0)
+#     case "desc":
+#         describe_note(int(arguments[1]),db_file)
+#         exit(0)
+#     case "new":
+#         create_note(db_file,arguments[1],arguments[2])
+#     case "rm":
+#         delete_note(int(arguments[1]),db_file)
+#         exit(0)
+#     case _:
+#         usage()
 
 
--- test_pnote.py
+++ test_pnote.py
@@ -8,17 +8,27 @@ class TestPnote(unittest.TestCase):
     
     @classmethod
     def setUpClass(cls):
-        self.working_dir = os.path.join('.','./fixtures/')
-        self.db_file = os.path.join(working_dir,'testdb.csv')
-        load_csv_database(self.working_dir,self.db_file)
+        cls.working_dir = os.path.join('.','./fixtures/')
+        cls.db_file = os.path.join(working_dir,'testdb.csv')
+        
     
     @classmethod
     def tearDownClass(cls):
-        os.removedirs(self.work)
-    
-    
-    def test_load_csv_database(self):
-        self.assert
+        os.removedirs(cls.working_dir)
 
+    def test_load_csv_database(self):
+        load_csv_database(self.working_dir,self.db_file)
+        
+        self.assertTrue(os.path.isdir(self.working_dir),"Must find the working dir.")
+        
+        self.assertTrue(os.path.isfile(self.db_file),"Must find the csv file.")
+
+        with open(self.db_file,'r') as f:
+            lines = f.readlines()
+        self.assertIn( 'id,datetime,title,body\n',lines,"Must find the csv headers in csv.")
+
+    def test_generate_note_id(self):
+        self.assertEqual(generate_note_id(self.db_file),1,"Must return ID of 1 because csv is empty.")
+    
 if __name__ == '__main__':
     unittest.main()
